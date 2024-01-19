8f7f9085742e17a34a4a505e7aabccdad7ef7835
litenite <lite.nite628@gmail.com>
2024-01-18 19:33:20 -0500

    fix run_prompt to read from stdin

--- _build/default/bin/main.ml
+++ _build/default/bin/main.ml
@@ -1,10 +1,25 @@
-let run_prompt () = print_endline "Running prog"
-let run_file x = Printf.printf "%s\n" x
+let read_lines file =
+let ch = open_in_bin file in
+    let s = really_input_string ch (in_channel_length ch) in
+    close_in ch;
+    s
+ 
+let run fileText = print_endline fileText
+
+let rec run_prompt () = 
+        Printf.printf "> ";
+match read_line () with
+| "" -> ()
+| x -> run x; run_prompt ()
+
+
+let run_file x = (*Printf.printf "%s\n" x*)
+        read_lines x |> run
 
 let () = 
         let argLen = Array.length Sys.argv 
         in
         match argLen with
-        0|1 -> run_prompt ()
-        | 2 -> run_file Sys.argv.(1)
-        | _ -> print_endline "Usage: ocamlVox [script]"
+        | 0|1 -> run_prompt ()
+        | 2   -> run_file Sys.argv.(1)
+        | _   -> print_endline "Usage: ocamlVox [script]";
--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,20 @@
-let run_prompt () = print_endline "Running prog"
-let run_file x = Printf.printf "%s\n" x
+let read_lines file =
+let ch = open_in_bin file in
+    let s = really_input_string ch (in_channel_length ch) in
+    close_in ch;
+    s
+ 
+let run fileText = print_endline fileText
+
+let rec run_prompt () = 
+        Printf.printf "> ";
+match read_line () with
+| "" -> ()
+| x -> run x; run_prompt ()
+
+
+let run_file x = (*Printf.printf "%s\n" x*)
+        read_lines x |> run
 
 let () = 
         let argLen = Array.length Sys.argv 
@@ -7,4 +22,4 @@ let () =
         match argLen with
         | 0|1 -> run_prompt ()
         | 2   -> run_file Sys.argv.(1)
-        | _   -> print_endline "Usage: ocamlVox [script]"
+        | _   -> print_endline "Usage: ocamlVox [script]";
